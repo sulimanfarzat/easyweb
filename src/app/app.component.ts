00fc57603ea622ba540e3279b206ae1369219eeb
Suliman Farzat <s_farzat@outlook.com>
2020-11-09 00:43:25 +0100

    12082020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { isModifier } from 'typescript';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easy2edi';
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    AOS.init();
+  }
 }
