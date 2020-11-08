a7febe17d9eefedecbe6354113dbc4d0a00e5f8f
Suliman Farzat <s_farzat@outlook.com>
2020-11-09 00:55:41 +0100

    09112020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,5 +74,5 @@ export class AppModule {
 
 // AOT compilation support
 export function httpTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
